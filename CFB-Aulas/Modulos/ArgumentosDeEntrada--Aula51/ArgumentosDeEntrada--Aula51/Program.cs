0129286ebf1d0aaae8e3afbcbe6d7b70f0774ec9
vagner-alves-backend <vagner.alves.dev@gmail.com>
2026-03-08 19:55:01 -0300

    Lendo os args passados no momento de inicialização do sistema.

---
+++ CFB-Aulas/Modulos/ArgumentosDeEntrada--Aula51/ArgumentosDeEntrada--Aula51/Program.cs
@@ -0,0 +1,11 @@
+﻿if (args.Length != 0)
+{
+    Console.WriteLine("Foram passados argumentos...");
+    foreach (string arg in args)
+    {
+        Console.WriteLine($"Argumento:\t{arg}");
+    }
+} else
+{
+    Console.WriteLine("Não foram passados argumentos.");
+}
