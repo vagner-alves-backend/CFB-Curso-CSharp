8567231a1911d315b95a52a11f1d45c9c218beed
vagner-alves-backend <vagner.alves.dev@gmail.com>
2026-03-18 19:33:16 -0300

    Lista encadeada simples... CFB-Cursos Aula 57....

--- CFB-Aulas/Listas/LinkedList/Program.cs
+++ CFB-Aulas/Listas/LinkedList/Program.cs
@@ -33,3 +33,32 @@ foreach (string? name in names)
     Console.WriteLine ($"\t{name}");
 }
 Console.WriteLine ("--------------------------");
+
+
+Console.WriteLine ("\t---Lista encadeada simples...");
+
+// Lista simples
+List <string?> names1 = [];
+
+// Adicionar nome
+names1.Add ("Vágner Alves");
+names1.Add ("Sofia Valverde");
+names1.Add ("Sabrine Gomes");
+
+// Copiando os elementos da lista anterior...
+List <string?> names2 = [];
+names2.AddRange (names);
+
+Console.WriteLine ("Lista 01...");
+foreach (string? name in names1)
+{
+    Console.WriteLine ($"\t-{name}");
+}
+Console.WriteLine ("......................");
+
+Console.WriteLine ("Lista 02...");
+foreach (string? name in names2)
+{
+    Console.WriteLine ($"\t-{name}");
+}
+Console.WriteLine ("......................");
