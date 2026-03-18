b5260fa1604cd84fda3304d62e6f3a10f2d6574f
vagner-alves-backend <vagner.alves.dev@gmail.com>
2026-03-18 12:45:10 -0300

    LinkedList com CFB-Cursos Aula 56

---
+++ CFB-Aulas/Listas/LinkedList/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear ();
+
+LinkedList <string?> names = [];
+
+// Adicionando nome no inicio da lista...
+names.AddFirst ("Vágner Alves");
+names.AddFirst ("Laura Gomes");
+names.AddFirst ("Debora Souza");
+
+// Adicionando nome no final da lista...
+names.AddLast ("Sofia Valverde");
+
+// Criando o no de referencia...
+LinkedListNode <string?>? no;
+
+// Adicionando um nome depois do nome "Vágner Alves"...
+no = names.FindLast ("Vágner Alves")?.Next; // Next faz com que ele aponte para o proximo elemento...
+if (no != null)
+{
+    names.AddAfter (no, "Luana Souza");
+}
+
+// Adicionando um nome antes do nome "Sofia Valverde"...
+no = names.FindLast ("Sofia Valverde")?.Previous; // Previous faz com que ele aponte para o elemento anterior...
+if (no != null)
+{
+    names.AddBefore (no, "Luciene Gomes");
+}
+
+Console.WriteLine ("---Lista de nomes...");
+foreach (string? name in names)
+{
+    Console.WriteLine ($"\t{name}");
+}
+Console.WriteLine ("--------------------------");
